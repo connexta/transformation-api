91303334e7b84580cc38754e7e439f659cdf4bd8
Kiefer Cover <kiefer.cover@connexta.com>
2019-11-01 10:36:03 -0700

    LORE-282 Remove OpenApi spec
    *Makes some changes for LORE-282
    *Removes the OpenApi spec since it does not meet our needs
    *Moves generated interfaces into source code directories
    
    *Removes the ErrorMessage field on MetadataTransformations and replaces
      it with a FailureReason and FailureMessage.
    *Changes the MetadataType to a String instead of an enum.

---
+++ models/src/main/java/com/connexta/transformation/rest/models/TransformRequest.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) Connexta
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package com.connexta.transformation.rest.models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+import java.net.URL;
+import java.util.Objects;
+import javax.validation.constraints.NotNull;
+
+/** TransformRequest */
+public class TransformRequest {
+  @JsonProperty("currentLocation")
+  private URL currentLocation;
+
+  @JsonProperty("finalLocation")
+  private URL finalLocation;
+
+  @JsonProperty("metacardLocation")
+  private URL metacardLocation;
+
+  public TransformRequest currentLocation(URL currentLocation) {
+    this.currentLocation = currentLocation;
+    return this;
+  }
+
+  /**
+   * This URL must support HTTP GET to retrieve the file. This MUST be available until the
+   * transformation request is \"Done\".
+   *
+   * @return currentLocation
+   */
+  @ApiModelProperty(
+      example = "https://www.example.com/mis/30f14c6c1fc85cba12bfd093aa8f90e3",
+      required = true,
+      value =
+          "This URL must support HTTP GET to retrieve the file. This MUST be available until the transformation request is \"Done\". ")
+  @NotNull
+  public URL getCurrentLocation() {
+    return currentLocation;
+  }
+
+  public void setCurrentLocation(URL currentLocation) {
+    this.currentLocation = currentLocation;
+  }
+
+  public TransformRequest finalLocation(URL finalLocation) {
+    this.finalLocation = finalLocation;
+    return this;
+  }
+
+  /**
+   * This URL will not actually be called by the Transform service. It will only be used by the
+   * Transform service to put in the transformed metadata for the \"resource download location\".
+   *
+   * @return finalLocation
+   */
+  @ApiModelProperty(
+      example = "https://www.example.com/mis/30f14c6c1fc85cba12bfd093aa8f90e3",
+      required = true,
+      value =
+          "This URL will not actually be called by the Transform service. It will only be used by the Transform service to put in the transformed metadata for the \"resource download location\". ")
+  @NotNull
+  public URL getFinalLocation() {
+    return finalLocation;
+  }
+
+  public void setFinalLocation(URL finalLocation) {
+    this.finalLocation = finalLocation;
+  }
+
+  public TransformRequest metacardLocation(URL metacardLocation) {
+    this.metacardLocation = metacardLocation;
+    return this;
+  }
+
+  /**
+   * This URL must support HTTP GET to retrieve the metacard xml.
+   *
+   * @return metacardLocation
+   */
+  @ApiModelProperty(
+      example = "https://www.example.com/mis/30f14c6c1fc85cba12bfd093aa8f90e3",
+      required = true,
+      value = "This URL must support HTTP GET to retrieve the metacard xml. ")
+  @NotNull
+  public URL getMetacardLocation() {
+    return metacardLocation;
+  }
+
+  public void setMetacardLocation(URL metacardLocation) {
+    this.metacardLocation = metacardLocation;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    TransformRequest transformRequest = (TransformRequest) o;
+    return Objects.equals(this.currentLocation, transformRequest.currentLocation)
+        && Objects.equals(this.finalLocation, transformRequest.finalLocation)
+        && Objects.equals(this.metacardLocation, transformRequest.metacardLocation);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(currentLocation, finalLocation, metacardLocation);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class TransformRequest {\n");
+
+    sb.append("    currentLocation: ").append(toIndentedString(currentLocation)).append("\n");
+    sb.append("    finalLocation: ").append(toIndentedString(finalLocation)).append("\n");
+    sb.append("    metacardLocation: ").append(toIndentedString(metacardLocation)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ spring/src/main/java/com/connexta/transformation/rest/spring/TransformApiRetrieve.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) Connexta
+ *
+ * <p>This is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or any later version.
+ *
+ * <p>This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details. A copy of the GNU Lesser General Public
+ * License is distributed along with this program and can be found at
+ * <http://www.gnu.org/licenses/lgpl.html>.
+ */
+package com.connexta.transformation.rest.spring;
+
+import com.connexta.transformation.rest.models.ErrorResponse;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Implementations of the {@code TransformApiRetrieve} interface provide a mechanism to implement
+ * the rest GET/retrieve endpoint.
+ */
+@Validated
+@Api(value = "transform", description = "the transform API")
+public interface TransformApiRetrieve {
+
+  @ApiOperation(
+      value = "Retrieve metadata.",
+      nickname = "retrieve",
+      notes = "Used to retrieve the metadata for a transform request.",
+      response = Resource.class,
+      tags = {
+        "transform",
+      })
+  @ApiResponses(
+      value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved.", response = Resource.class),
+        @ApiResponse(
+            code = 400,
+            message =
+                "The client message could not be understood by the server due to invalid format or syntax.",
+            response = ErrorResponse.class),
+        @ApiResponse(
+            code = 401,
+            message = "The client could not be authenticated.",
+            response = ErrorResponse.class),
+        @ApiResponse(
+            code = 403,
+            message = "The client is not authorized.",
+            response = ErrorResponse.class),
+        @ApiResponse(
+            code = 404,
+            message = "The transform request could not be found.",
+            response = ErrorResponse.class),
+        @ApiResponse(
+            code = 501,
+            message =
+                "The requested API version is not supported and therefore not implemented. Possible codes reported are: - 501001 - Unable to parse *Accept-Version* - 501002 - The provided major version is no longer supported - 501003 - The provided major version is not yet supported by the server - 501004 - The provided minor version is not yet supported by the server ",
+            response = ErrorResponse.class),
+        @ApiResponse(
+            code = 200,
+            message = "Any other possible errors not currently known.",
+            response = ErrorResponse.class)
+      })
+  @RequestMapping(
+      value = "/transform/{TransformId}/{MetadataType}",
+      produces = {"application/octet-stream", "application/json"},
+      method = RequestMethod.GET)
+  default ResponseEntity<Resource> retrieve(
+      @ApiParam(value = "The ID of the transform request. ", required = true)
+          @PathVariable("TransformId")
+          String transformId,
+      @ApiParam(value = "The metadata format ID.", required = true, allowableValues = "\"irm\"")
+          @PathVariable("MetadataType")
+          String metadataType)
+      throws Exception {
+    return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
+  }
+}
